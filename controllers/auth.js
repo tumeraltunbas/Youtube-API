782ab90ac60fecff72a99b91ecdfc086b1773641
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-13 15:51:49 +0300

    email verification endpoint and register refactored

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
 import { createMailOptions, sendMail } from "../helpers/email/emailHelpers.js";
+import CustomizedError from "../helpers/error/CustomizedError.js";
+import { sendEmailVerificationCode } from "../helpers/modelHelpers/modelHelpers.js";
 import User from "../models/User.js";
 export const register = async(req, res, next) => {
     try{
         const {firstName, lastName, email, password, gender} = req.body;
-        const user = await User.create({firstName:firstName, lastName:lastName, email:email, password:password, gender:gender});
-        const verificationCode = await user.emailVerification();
-        sendMail(createMailOptions(email, "Email confirmation", `<p>Your email verification code is ${verificationCode}. This code is only valid for 10 minutes</p>`));
-        return res.status(200).json({success:true, message:`Email verification code sent to ${email}`});
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+        if(!passwordRegex.test(password))
+        return next(new CustomizedError(400,"Password must contain: Minimum eight characters, at least one uppercase letter, one lowercase letter and one number"));
+        const user = await User.create({firstName:firstName, lastName:lastName, email:email, password:password, gender:gender});       
+        await sendEmailVerificationCode(user); 
+        return res.status(200).json({success:true, message:`Email verification code sent to ${email}`, data:user});
     }
     catch(err){
+        return next(err);
+    }
+}
+
+export const emailVerification = async(req, res, next) => {
+    try {
+        const {emailVerificationCode} = req.body;
+        const user = await User.findOne(
+            {$and : [
+                {emailVerificationCode:emailVerificationCode}, 
+                {emailVerificationCodeExpires: {$gt: Date.now()}}]})
+                .select("isEmailVerified emailVerificationCode emailVerificationCodeExpires");
+        if(!user)
+        return next(new CustomizedError(400, "Your verification code wrong or expired."));
+        await user.update({emailVerificationCode:null, emailVerificationCodeExpires:null, isEmailVerified:true});
+        return res.status(200).json({success:true, message:"Your email has been verified."});
+    }
+    catch(err) {
         return next(err);
     }
 }

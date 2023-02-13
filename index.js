ec72c4d90894c9fb2edad97d0da76d7a10d7928d
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-13 13:24:32 +0300

    routes imported

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import connectDatabase from "./helpers/database/connectionHelper.js";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/error/errorHandler.js";
+import routes from "./routes/index.js";
 dotenv.config({path:"./config/.env"});
 const app = express(); //an app created from express's constructor.
 connectDatabase();
 app.use(express.json()); //body-parser
-
+app.use("/", routes);
 app.use(errorHandler);
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));

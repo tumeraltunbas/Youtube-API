e7c917dcbccf4bc706af9419f445e63e4b8c62a6
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-13 15:51:34 +0300

    email verification endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 import {Router} from "express";
-import { register } from "../controllers/auth.js";
+import { register,emailVerification } from "../controllers/auth.js";
 
 const router = Router();
 router.post("/register", register);
+router.post("/email-verification", emailVerification);
 
 export default router;

5ce72186850a69bf2b3834ea148ddc0471c11913
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-13 13:23:11 +0300

    errorHandler added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import connectDatabase from "./helpers/database/connectionHelper.js";
 import dotenv from "dotenv";
+import errorHandler from "./middlewares/error/errorHandler.js";
 dotenv.config({path:"./config/.env"});
 const app = express(); //an app created from express's constructor.
 connectDatabase();
 app.use(express.json()); //body-parser
+
+app.use(errorHandler);
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));

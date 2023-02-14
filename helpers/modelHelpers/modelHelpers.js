592c0b8390a73657aab745e1f10064d4b7e1ee02
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-14 16:07:51 +0300

    promise fix

--- helpers/modelHelpers/modelHelpers.js
+++ helpers/modelHelpers/modelHelpers.js
@@ -2,12 +2,12 @@ import { createMailOptions, sendMail } from "../email/emailHelpers.js";
 
 export const sendEmailVerificationCode = async(user) => 
 {
-    const verificationCode = user.emailVerification();
+    const verificationCode = await user.emailVerification();
     sendMail(createMailOptions(user.email, "Email confirmation", `<p>Your verification code is ${verificationCode}. This code is only valid for 10 minutes</p>`));
 }
 
-export const sendResetPasswordLink = (user) => {
+export const sendResetPasswordLink = async(user) => {
     const {DOMAIN} = process.env;
-    const verificationCode = user.resetPassword();
+    const verificationCode = await user.resetPassword();
     sendMail(createMailOptions(user.email, "Reset Password", `<p>Your reset password <a href='${DOMAIN}/auth/resetPassword?resetPasswordToken=${verificationCode}'>link</a>. This link is only valid for 10 minutes</p>`));
 }

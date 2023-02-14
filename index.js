f33cc8f2332e70bcd6247f37eebed68f5778242b
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-14 16:53:52 +0300

    file upload imported

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 import errorHandler from "./middlewares/error/errorHandler.js";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 dotenv.config({path:"./config/.env"});
 const app = express(); //an app created from express's constructor.
 connectDatabase();
 app.use(express.json()); //body-parser
 app.use(cookieParser()); //cookie-parser
+app.use(express.static("./public"));
+app.use(fileUpload({limits:{fileSize: 5*1024*1024}}));
 app.use("/", routes);
 app.use(errorHandler);
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));

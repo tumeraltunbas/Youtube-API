eec5a2da33704bc77b1f6a4ab1d26374e5f7c352
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-14 12:53:22 +0300

    cookie parser added

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import connectDatabase from "./helpers/database/connectionHelper.js";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/error/errorHandler.js";
 import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config({path:"./config/.env"});
 const app = express(); //an app created from express's constructor.
 connectDatabase();
 app.use(express.json()); //body-parser
+app.use(cookieParser()); //cookie-parser
 app.use("/", routes);
 app.use(errorHandler);
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));

4a75924fe3ecb30df924a637be33b8bc14ace677
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-14 13:25:31 +0300

    send reset password code function

--- helpers/modelHelpers/modelHelpers.js
+++ helpers/modelHelpers/modelHelpers.js
@@ -5,3 +5,8 @@ export const sendEmailVerificationCode = async(user) =>
     const verificationCode = user.emailVerification();
     sendMail(createMailOptions(user.email, "Email confirmation", `<p>Your verification code is ${verificationCode}. This code is only valid for 10 minutes</p>`));
 }
+
+export const sendResetPasswordCode = (user) => {
+    const verificationCode = user.resetPassword();
+    sendMail(createMailOptions(user.email, "Reset Password", `<p>Your Reset password code is ${verificationCode}. This code is only valid for 10 minutes</p>`));
+}

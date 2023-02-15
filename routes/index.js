6d9e69f7c41cec2a0ea917bbcd6172db1d096ee0
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-15 12:20:34 +0300

    channel routes added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import {Router} from "express";
 import authRoutes from "./auth.js";
+import channelRoutes from "./channel.js";
 
 const router = Router();
 router.use("/auth", authRoutes);
+router.use("/channel", channelRoutes);
 
 export default router;

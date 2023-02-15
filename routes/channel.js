f1a2534ab16aaf8364b3cc09032e9836202a4e46
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-15 13:11:09 +0300

    get channel endpoion

--- routes/channel.js
+++ routes/channel.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { createChannel,uploadChannelProfilePhoto,editChannelInformations } from "../controllers/channel.js";
+import { createChannel,uploadChannelProfilePhoto,editChannelInformations,getChannel } from "../controllers/channel.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
 
 const router = Router();
 router.post("/create", getAccessToRoute, createChannel);
 router.post("/profile-photo", getAccessToRoute, uploadChannelProfilePhoto);
 router.put("/edit", getAccessToRoute, editChannelInformations);
+router.get("/:slug", getChannel);
+
 export default router;

b710fa2fb6be1f17f87c432e9a592f6776d9b703
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-16 11:23:22 +0300

    video routes added

--- routes/channel.js
+++ routes/channel.js
@@ -1,11 +1,13 @@
 import {Router} from "express";
 import { createChannel,uploadChannelProfilePhoto,editChannelInformations,getChannel } from "../controllers/channel.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
+import videoRoutes from "./video.js";
 
 const router = Router();
 router.post("/create", getAccessToRoute, createChannel);
 router.post("/profile-photo", getAccessToRoute, uploadChannelProfilePhoto);
 router.put("/edit", getAccessToRoute, editChannelInformations);
 router.get("/:slug", getChannel);
+router.use("/:slug", videoRoutes);
 
 export default router;

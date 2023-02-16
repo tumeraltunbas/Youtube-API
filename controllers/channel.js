eb39818ce52c20fd14d7966608e36ca2da512233
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-16 14:27:00 +0300

    subscribe endpoint

--- controllers/channel.js
+++ controllers/channel.js
@@ -50,3 +50,19 @@ export const getChannel = async(req, res, next) => {
         return next(err);
     }
 }
+
+export const subscribe = async(req, res, next) => {
+    try {
+        const {channelSlug} = req.params;
+        const channel = await Channel.findOne({slug:channelSlug}).select("subscribers subscribeCount");
+        if(channel.subscribers.includes(req.user.id))
+        return next(new CustomizedError(400, "You already subscribed to this channel"));
+        channel.subscribers.push(req.user.id);
+        channel.subscribeCount +=1;
+        await channel.save();
+        return res.status(200).json({success:true, message:"Subscribe successfull"});
+    }
+    catch(err) {
+        return next(err);
+    }
+}
--- routes/channel.js
+++ routes/channel.js
@@ -1,12 +1,14 @@
 import {Router} from "express";
-import { createChannel,uploadChannelProfilePhoto,editChannelInformations,getChannel } from "../controllers/channel.js";
+import { createChannel,uploadChannelProfilePhoto,editChannelInformations,getChannel, subscribe } from "../controllers/channel.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
+import { isChannelExist } from "../middlewares/query/queryMiddleware.js";
 import videoRoutes from "./video.js";
 
 const router = Router();
 router.post("/create", getAccessToRoute, createChannel);
 router.post("/profile-photo", getAccessToRoute, uploadChannelProfilePhoto);
 router.put("/edit", getAccessToRoute, editChannelInformations);
+router.get("/subscribe/:channelSlug", [getAccessToRoute, isChannelExist], subscribe);
 router.get("/:channelSlug", getChannel);
 router.use("/:channelSlug", videoRoutes);
 

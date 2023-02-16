dad1bf4d7ce49fdc05a61a58a44a7f6d349629e2
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-16 12:04:20 +0300

    edit video endpoint

--- routes/video.js
+++ routes/video.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { uploadVideo } from "../controllers/video.js";
-import { getAccessToRoute } from "../middlewares/auth/auth.js";
+import { editVideo, uploadVideo } from "../controllers/video.js";
+import { getAccessToRoute, getVideoOwnerAccess } from "../middlewares/auth/auth.js";
+import { isVideoExists } from "../middlewares/query/queryMiddleware.js";
 
 const router = Router({mergeParams:true});
 router.post("/upload", getAccessToRoute, uploadVideo);
+router.put("/edit/:videoSlug", [getAccessToRoute, isVideoExists ,getVideoOwnerAccess], editVideo);
 
 
 export default router;

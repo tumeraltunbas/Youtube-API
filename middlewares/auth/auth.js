c85d2d5f665d94a236827d069da7aa0c0de8f2ff
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-17 11:48:04 +0300

    getCommentOwnerAccess middleware

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -3,6 +3,7 @@ import User from "../../models/User.js";
 import jwt from "jsonwebtoken";
 import Video from "../../models/Video.js";
 import Channel from "../../models/Channel.js";
+import Comment from "../../models/Comment.js";
 
 export const isEmailVerified = async(req, res, next) => {
     try {
@@ -41,6 +42,19 @@ export const getVideoOwnerAccess = async(req, res, next) => {
         return next(new CustomizedError(403, "You are not owner of video"));
         next();
     } 
+    catch(err) {
+        return next(err);
+    }
+}
+
+export const getCommentOwnerAccess = async(req, res, next) => {
+    try {
+        const {commentId} = req.params;
+        const comment = await Comment.findById(commentId).select("_id user");
+        if(comment.user != req.user.id)
+        return next(new CustomizedError(400, "You are not owner this comment"));
+        next();
+    }
     catch(err) {
         return next(err);
     }

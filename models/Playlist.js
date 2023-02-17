1afe3783a0b87fb949bf7d3190e61e871edeb17a
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-17 13:48:49 +0300

    Playlist model created

---
+++ models/Playlist.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const PlaylistSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, "Name can not be null"]
+    },
+    videos : [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref:"Video"
+        }
+    ],
+    privacy: {
+        type:String,
+        enum: ["public", "private"],
+        required:[true, "Privacy can not be null"],
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now()
+    },
+});
+
+const Playlist = mongoose.model("Playlist", PlaylistSchema);
+export default Playlist;

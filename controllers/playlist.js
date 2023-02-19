0df2fd4889c15885ed6f43bc3eae2c078df49445
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-19 14:36:53 +0300

    hide playlist endpoint

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -3,11 +3,22 @@ import Playlist from "../models/Playlist.js";
 export const createPlaylist = async(req, res, next) => {
     try{
         const {name, privacy} = req.body;
-        await Playlist.create({name:name, privacy:privacy});
+        await Playlist.create({name:name, privacy:privacy, user:req.user.id});
         return res.status(200).json({success:true, message: "Playlist has been created"});
     }
     catch(err)
     {
+        return next(err);
+    }
+}
+
+export const hidePlaylist = async(req, res, next) => {
+    try {
+        const {playlistId} = req.params;
+        await Playlist.updateOne({_id:playlistId}, {isVisible:false}, {new:true});
+        return res.status(200).json({success:true, message:"Your playlist has been deleted"});
+    }
+    catch(err) {
         return next(err);
     }
 }
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
-import { createPlaylist } from "../controllers/playlist.js";
-import {getAccessToRoute} from "../middlewares/auth/auth.js";
+import { createPlaylist, hidePlaylist } from "../controllers/playlist.js";
+import {getAccessToRoute, getPlaylistOwnerAccess} from "../middlewares/auth/auth.js";
+import { isPlaylistExists } from "../middlewares/query/queryMiddleware.js";
+
 const router = Router();
+
 router.use(getAccessToRoute);
-router.post("/create", createPlaylist);
+router.post("/create", getAccessToRoute, createPlaylist);
+router.put("/hide/:playlistId", [getAccessToRoute, isPlaylistExists, getPlaylistOwnerAccess], hidePlaylist);
 
 export default router;

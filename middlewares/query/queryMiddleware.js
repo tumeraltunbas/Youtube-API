bee89a6ee8285f0609766bce295fd8cc3b8aae3b
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-19 15:55:11 +0300

    query refactoring

--- middlewares/query/queryMiddleware.js
+++ middlewares/query/queryMiddleware.js
@@ -6,7 +6,9 @@ import Video from "../../models/Video.js";
 export const isVideoExists = async(req, res, next) => {
     try{
         const {channelSlug, videoSlug} = req.params;
-        const channel = await Channel.findOne({slug:channelSlug}).select("_id");
+        const channel = await Channel.findOne({slug:channelSlug, isVisible:true}).select("_id");
+        if(!channel)
+        return next(new CustomizedError(400, "There is no channel with this slug"));
         if(!await Video.findOne({slug:videoSlug, channel:channel.id}))
         return next(new CustomizedError(400, "There is no video like this"));
         next();
@@ -19,7 +21,7 @@ export const isVideoExists = async(req, res, next) => {
 export const isChannelExist = async(req, res, next) => {
     try {
         const {channelSlug} = req.params;
-        if(!await Channel.findOne({slug:channelSlug}).select("_id"))
+        if(!await Channel.findOne({slug:channelSlug, isVisible:true}).select("_id"))
         return next(new CustomizedError(400, "There is no channel with this slug"));
         next();
     }
@@ -31,7 +33,7 @@ export const isChannelExist = async(req, res, next) => {
 export const isCommentExists = async(req, res, next) => {
     try {
         const {videoSlug, commentId} = req.params;
-        const video = await Video.findOne({slug:videoSlug}).select("_id comments");
+        const video = await Video.findOne({slug:videoSlug, isVisible:true}).select("_id comments");
         if(!video.comments.includes(commentId))
         return next(new CustomizedError(400, "There is no comment in this video"));
         next();
@@ -44,7 +46,7 @@ export const isCommentExists = async(req, res, next) => {
 export const isPlaylistExists = async(req, res, next) => {
     try{
         const {playlistId} = req.params;
-        if(!await Playlist.findById(playlistId).select("_id"))
+        if(!await Playlist.findOne({_id:playlistId, isVisible:true}).select("_id"))
         return next(new CustomizedError(400, "There is no playlist with that id"));
         next();
     }
@@ -56,7 +58,7 @@ export const isPlaylistExists = async(req, res, next) => {
 export const isVideoExistsInPlaylist = async(req, res, next) => {
     try{
         const {videoSlug, playlistId} = req.params;
-        const playlist = await Playlist.findById(playlistId).select("_id videos");
+        const playlist = await Playlist.findOne({_id: playlistId, isVisible:true}).select("_id videos");
         const video = await Video.findOne({slug:videoSlug}).select("_id");
         if(!playlist.videos.includes(video.id))
         return next(new CustomizedError(400, "There is no video in this playlist"));

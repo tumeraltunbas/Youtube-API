8414ba32292eabefb64b157b990ae6970549d7c4
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-19 15:04:42 +0300

    add video to playlist endpoint

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,4 +1,6 @@
+import CustomizedError from "../helpers/error/CustomizedError.js";
 import Playlist from "../models/Playlist.js";
+import Video from "../models/Video.js";
 
 export const createPlaylist = async(req, res, next) => {
     try{
@@ -19,6 +21,22 @@ export const hidePlaylist = async(req, res, next) => {
         return res.status(200).json({success:true, message:"Your playlist has been deleted"});
     }
     catch(err) {
+        return next(err);
+    }
+}
+
+export const addVideoToPlaylist = async(req, res, next) => {
+    try{
+        const {videoSlug, playlistId} = req.params;
+        const video = await Video.findOne({slug:videoSlug}).select("_id");
+        const playlist = await Playlist.findById(playlistId).select("_id videos");
+        if(playlist.videos.includes(video.id))
+        return next(new CustomizedError(400, "This video is already available on this playlist"));
+        playlist.videos.push(video.id);
+        await playlist.save();
+        return res.status(200).json({success:true, message:"Video has been added to playlist"});
+    }
+    catch(err){
         return next(err);
     }
 }
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,12 +1,13 @@
 import {Router} from "express";
-import { createPlaylist, hidePlaylist } from "../controllers/playlist.js";
+import { createPlaylist, hidePlaylist,addVideoToPlaylist } from "../controllers/playlist.js";
 import {getAccessToRoute, getPlaylistOwnerAccess} from "../middlewares/auth/auth.js";
-import { isPlaylistExists } from "../middlewares/query/queryMiddleware.js";
+import { isPlaylistExists, isVideoExists } from "../middlewares/query/queryMiddleware.js";
 
-const router = Router();
+const router = Router({mergeParams:true});
 
 router.use(getAccessToRoute);
 router.post("/create", getAccessToRoute, createPlaylist);
 router.put("/hide/:playlistId", [getAccessToRoute, isPlaylistExists, getPlaylistOwnerAccess], hidePlaylist);
+router.get("/add/:playlistId", [getAccessToRoute, isVideoExists, isPlaylistExists], addVideoToPlaylist);
 
 export default router;

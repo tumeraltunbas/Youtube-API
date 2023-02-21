feca161e092560c23d54896e605935aab354a7eb
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 16:30:10 +0300

    report confirm endpoint

--- controllers/staff.js
+++ controllers/staff.js
@@ -2,6 +2,9 @@ import { createMailOptions, sendMail } from "../helpers/email/emailHelpers.js";
 import Channel from "../models/Channel.js";
 import ChannelVerification from "../models/ChannelVerification.js";
 import Report from "../models/Report.js";
+import Video from "../models/Video.js";
+import User from "../models/User.js";
+
 export const verificationRequests = async(req, res, next) => {
     try{
         const verificationRequests = await ChannelVerification.find({isVisible:true});
@@ -44,6 +47,24 @@ export const getReports = async(req, res, next) => {
     try{
         const reports = await Report.find({isVisible:true}).populate({path: "video", select:"title video viewCount likeCount dislikeCount"}).populate({path:"user", select:"firstName lastName"});
         return res.status(200).json({success:true, data:reports});
+    }
+    catch(err){
+        return next(err);
+    }
+}
+
+export const confirmReport = async(req, res, next) => {
+    try{
+        const {reportId} = req.params;
+        const report = await Report.findOne({_id:reportId}).select("_id video user reason"); 
+        const video = await Video.findOne({_id: report.video}).select("_id channel");
+        const channel = await Channel.findOne({_id:video.channel}).select("_id email");
+        const user = await User.findOne({_id:report.user}).select("_id firstName lastName email");
+        await video.updateOne({isVisible:false});
+        await report.updateOne({isVisible:false});
+        sendMail(createMailOptions(channel.email, "Your video has been deleted", `<p>Your video has been deleted from Youtube due to ${report.reason}</p>`));
+        sendMail(createMailOptions(user.email, "Thanks for report", `<p>Dear ${user.firstName} ${user.lastName}, Your report was received positively by us and the related video was removed from YouTube.</p>`));
+        res.status(200).json({success:true, message:"Report has been confirmed"});        
     }
     catch(err){
         return next(err);
--- routes/staff.js
+++ routes/staff.js
@@ -1,12 +1,13 @@
 import {Router} from "express";
-import { getReports, refuseChannelVerification, verificationRequests, verifyChannel } from "../controllers/staff.js";
+import { verificationRequests, verifyChannel, refuseChannelVerification, getReports, confirmReport } from "../controllers/staff.js";
 import {getAccessToRoute, getStaffAccess} from "../middlewares/auth/auth.js";
-import { isVerificationRequestExists } from "../middlewares/query/queryMiddleware.js";
+import { isReportExists, isVerificationRequestExists } from "../middlewares/query/queryMiddleware.js";
 
 const router = Router();
 router.get("/verification-requests", [getAccessToRoute, getStaffAccess], verificationRequests);
 router.get("/verification-requests/:channelId/verify", [getAccessToRoute, getStaffAccess, isVerificationRequestExists], verifyChannel);
 router.get("/verification-requests/:channelId/refuse", [getAccessToRoute, getStaffAccess, isVerificationRequestExists], refuseChannelVerification);
 router.get("/reports", [getAccessToRoute, getStaffAccess], getReports);
+router.get("/reports/:reportId/confirm", [getAccessToRoute, getStaffAccess, isReportExists], confirmReport);
 
 export default router;

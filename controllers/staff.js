3325259b26bb229e1555d33e35afdbc72a6c7ea3
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 15:14:09 +0300

    mail functionality injected

--- controllers/staff.js
+++ controllers/staff.js
@@ -15,19 +15,23 @@ export const verificationRequests = async(req, res, next) => {
 export const verifyChannel = async(req, res, next) => {
     try{
         const {channelId} = req.params;
-        await ChannelVerification.updateOne({channel:channelId}, {isVisible:false}, {new:true});
-        await Channel.updateOne({_id:channelId, isVisible:true}, {isChannelVerified:true}, {new:true});
+        const channel = await Channel.findOne({_id:channelId}).select("_id email");
+        await ChannelVerification.updateOne({channel:channel.id}, {isVisible:false}, {new:true});
+        await channel.updateOne({isChannelVerified:true}, {new:true});
+        sendMail(createMailOptions(channel.email, 'Channel Verification Request', '<p>Your channel verification request has been positive. Your channel has been successfully verified.</p>'));
         return res.status(200).json({success:true, message:"Channel has been verified"});
     }
     catch(err){
         return next(err);
     }
 }
-//isteklerinin kabul edildiğine veya reddedildiğine dair mail gönder
+
 export const refuseChannelVerification = async(req, res, next) => {
     try{
         const {channelId} = req.params;
+        const channel = await Channel.findOne({_id:channelId}).select("_id email");
         await ChannelVerification.updateOne({channel:channelId}, {isVisible:false}, {new:true});
+        sendMail(createMailOptions(channel.email, 'Channel Verification Request', '<p>Your channel verification request has been negative. Your channel has not been verified.</p>'));
         return res.status(200).json({success:true, message:"Verification request has been refused"});
     }
     catch(err){

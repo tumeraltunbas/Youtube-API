d9fdd2143964a27950b70c0dad099088bc74fc73
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 16:47:17 +0300

    user routes added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./auth.js";
 import channelRoutes from "./channel.js";
 import playlistRoutes from "./playlist.js";
 import staffRoutes from "./staff.js";
+import userRoutes from "./user.js";
 import { index, trends, searchVideo } from "../controllers/index.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
 
@@ -13,5 +14,6 @@ router.use("/auth", authRoutes);
 router.use("/channel", channelRoutes);
 router.use("/playlist", playlistRoutes);
 router.use("/staff", staffRoutes);
+router.use("/user", userRoutes);
 router.get("/search",[getAccessToRoute], searchVideo);
 export default router;

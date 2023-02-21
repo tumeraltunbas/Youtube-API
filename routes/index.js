586abe153a7f78b3cf30c12b8ba17345c12ee9d7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 14:02:29 +0300

    staff routes added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import authRoutes from "./auth.js";
 import channelRoutes from "./channel.js";
+import playlistRoutes from "./playlist.js";
+import staffRoutes from "./staff.js";
 import { index, trends, searchVideo } from "../controllers/index.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
-import playlistRoutes from "./playlist.js";
 
 const router = Router();
 router.get("/", getAccessToRoute, index);
@@ -11,5 +12,6 @@ router.get("/trends", getAccessToRoute, trends);
 router.use("/auth", authRoutes);
 router.use("/channel", channelRoutes);
 router.use("/playlist", playlistRoutes);
+router.use("/staff", staffRoutes);
 router.get("/search",[getAccessToRoute], searchVideo);
 export default router;

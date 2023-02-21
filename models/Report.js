8d1fce6fb7b7b222344505092dcd090997a59e01
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 15:38:07 +0300

    Report model created

---
+++ models/Report.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ReportSchema = new mongoose.Schema({
+    video: {
+        type:mongoose.Schema.ObjectId,
+        required:[true, "Video can not be null"],
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        required:[true, "User can not be null"]
+    },
+    reason: {
+        type: String,
+        enum: ["Violent content", "Tyranny", "Spam or misleading content", "It violates my rights", "Problem is not stated here"],
+        required:[true, "Reason can not be null"]
+    },
+    details: {
+        type:String,
+        default:null,
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now()
+    }
+});
+
+const Report = mongoose.model("Report", ReportSchema);
+export default Report;

e22529a4f0d7369e27859668217e1cbe942f5a85
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 16:04:00 +0300

    getReports endpoint

--- controllers/staff.js
+++ controllers/staff.js
@@ -1,6 +1,7 @@
 import { createMailOptions, sendMail } from "../helpers/email/emailHelpers.js";
 import Channel from "../models/Channel.js";
 import ChannelVerification from "../models/ChannelVerification.js";
+import Report from "../models/Report.js";
 export const verificationRequests = async(req, res, next) => {
     try{
         const verificationRequests = await ChannelVerification.find({isVisible:true});
@@ -33,6 +34,16 @@ export const refuseChannelVerification = async(req, res, next) => {
         await ChannelVerification.updateOne({channel:channelId}, {isVisible:false}, {new:true});
         sendMail(createMailOptions(channel.email, 'Channel Verification Request', '<p>Your channel verification request has been negative. Your channel has not been verified.</p>'));
         return res.status(200).json({success:true, message:"Verification request has been refused"});
+    }
+    catch(err){
+        return next(err);
+    }
+}
+
+export const getReports = async(req, res, next) => {
+    try{
+        const reports = await Report.find({isVisible:true}).populate({path: "video", select:"title video viewCount likeCount dislikeCount"}).populate({path:"user", select:"firstName lastName"});
+        return res.status(200).json({success:true, data:reports});
     }
     catch(err){
         return next(err);
--- routes/staff.js
+++ routes/staff.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { refuseChannelVerification, verificationRequests, verifyChannel } from "../controllers/staff.js";
+import { getReports, refuseChannelVerification, verificationRequests, verifyChannel } from "../controllers/staff.js";
 import {getAccessToRoute, getStaffAccess} from "../middlewares/auth/auth.js";
 import { isVerificationRequestExists } from "../middlewares/query/queryMiddleware.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/verification-requests", [getAccessToRoute, getStaffAccess], verificationRequests);
 router.get("/verification-requests/:channelId/verify", [getAccessToRoute, getStaffAccess, isVerificationRequestExists], verifyChannel);
 router.get("/verification-requests/:channelId/refuse", [getAccessToRoute, getStaffAccess, isVerificationRequestExists], refuseChannelVerification);
+router.get("/reports", [getAccessToRoute, getStaffAccess], getReports);
 
 export default router;

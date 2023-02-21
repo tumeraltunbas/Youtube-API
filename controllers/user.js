126b2c570c35a0098b2b6334a44ff736cb08cde4
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-21 17:03:11 +0300

    get subscriptions endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+import Channel from "../models/Channel.js";
 import Video from "../models/Video.js";
 
 export const getLikedVideos = async(req, res, next) => {
@@ -14,6 +15,16 @@ export const getWatchHistory = async(req, res, next) => {
     try{
         const videos = await Video.find({views:req.user.id}).select("_id title thumbnail viewCount createdAt channel").sort({createdAt:"desc"});
         return res.status(200).json({success:true, data:videos});
+    }
+    catch(err){
+        return next(err);
+    }
+}
+
+export const getSubscriptions = async(req, res, next) => {
+    try{
+        const subscriptions = await Channel.find({subscribers:req.user.id}).select("_id name channelProfilePhoto");
+        return res.status(200).json({success:true, data:subscriptions});
     }
     catch(err){
         return next(err);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { getLikedVideos, getWatchHistory } from "../controllers/user.js";
+import { getLikedVideos, getWatchHistory,getSubscriptions } from "../controllers/user.js";
 import { getAccessToRoute } from "../middlewares/auth/auth.js";
 
 const router = Router();
 router.get("/liked-videos", [getAccessToRoute], getLikedVideos);
 router.get("/watch-history", [getAccessToRoute], getWatchHistory);
+router.get("/subscriptions", [getAccessToRoute], getSubscriptions);
 export default router;

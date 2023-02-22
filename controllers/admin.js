5d604d157ffbb792d802bc152158f716b51a5db7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-22 11:31:47 +0300

    getUserById endpoint

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,3 +11,13 @@ export const getAllUsers = async(req, res, next) => {
     }
 }
 
+export const getUserById = async(req, res, next) => {
+    try{ 
+        const {userId} = req.params; 
+        const user = await User.findOne({_id:userId}); 
+        return res.status(200).json({success:true, data:user});
+    } 
+    catch(err){
+        return next(err);
+    }
+}             
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { getAllUsers } from "../controllers/admin.js";
+import { getAllUsers, getUserById } from "../controllers/admin.js";
 import {getAccessToRoute, getAdminAccess} from "../middlewares/auth/auth.js";
+import { isUserExists } from "../middlewares/query/queryMiddleware.js";
 
 const router = Router();
 router.use([getAccessToRoute, getAdminAccess]);
 router.get("/users", getAllUsers);
+router.get("/users/:userId", isUserExists, getUserById);
 
 export default router;
